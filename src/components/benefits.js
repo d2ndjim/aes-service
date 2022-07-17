637fb8fc0fc5d7cfecd9e20a8ea6162f186c1645
Lekan <jimoridwanlekan@gmail.com>
2022-07-17 10:33:41 +0100

    make benefits images responsive

--- src/components/benefits.js
+++ src/components/benefits.js
@@ -11,77 +11,70 @@ const Benefits = () => (
         <BsLightningCharge className="text-4xl text-[#F7B736]" />
         <h3>Our Benefits</h3>
       </div>
-      <h1 className="text-[#25262E] text-3xl font-extrabold text-center mb-8">
+      <h1 className="text-[#25262E] md:text-3xl text-xl font-bold md:font-extrabold text-center mb-8">
         Why We Should Be Your Top
         <br />
         {' '}
         Choice
       </h1>
-      <div className="grid grid-cols-5 gap-4 w-4/5">
+      <div className="flex flex-col md:grid md:grid-cols-5 gap-4 w-4/5">
         <div className="flex flex-col gap-4 items-center justify-center border-solid border-2 border-[#E5E5E5] p-6 hover:border-[#F7B736]">
-          <TbHourglassLow className="text-[#F7B736] text-3xl" />
+          <TbHourglassLow className="text-[#F7B736] text-4xl md:text-3xl" />
           <p className="ben text-[#25262E]">Quick Response Times</p>
         </div>
         <div className="flex flex-col gap-4 items-center justify-center border-solid border-2 border-[#E5E5E5] p-8 hover:border-[#F7B736]">
-          <TbLicense className="text-[#F7B736] text-3xl" />
+          <TbLicense className="text-[#F7B736] text-4xl md:text-3xl" />
           <p className="ben text-[#25262E]">Licensed Electricians</p>
         </div>
         <div className="flex flex-col gap-4 items-center justify-center border-solid border-2 border-[#E5E5E5] p-8 hover:border-[#F7B736]">
-          <GrUserExpert className="text-[#F7B736] text-3xl" />
+          <GrUserExpert className="text-[#F7B736] text-4xl md:text-3xl" />
           <p className="ben text-[#25262E] text-center">
             Experienced and Accesible
           </p>
         </div>
         <div className="flex flex-col gap-4 items-center justify-center border-solid border-2 border-[#E5E5E5] p-8 hover:border-[#F7B736]">
-          <BsTrophy className="text-[#F7B736] text-3xl" />
+          <BsTrophy className="text-[#F7B736] text-4xl md:text-3xl" />
           <p className="ben text-[#25262E]">Super Service Award</p>
         </div>
         <div className="flex flex-col gap-4 items-center justify-center border-solid border-2 border-[#E5E5E5] p-8 hover:border-[#F7B736]">
-          <IoIosTimer className="text-[#F7B736] text-3xl" />
+          <IoIosTimer className="text-[#F7B736] text-4xl md:text-3xl" />
           <p className="ben text-[#25262E]">10 years+ experience</p>
         </div>
       </div>
     </div>
-    <div className="flex mb-6">
+    <div className="flex flex-col md:flex md:flex-row mb-6 img">
       <div className="wiring flex flex-col justify-between text-white items-center">
-        <h2 className="font-extrabold text-2xl mx-8 mt-16">
+        <h2 className="font-extrabold mx-8 mt-16">
           Electrical Wiring And Re-Wiring Experts
         </h2>
-        <div className="mb-12">
+        <div className="flex gap-3 items-center justify-center p-8 md:p-12">
           <div className="vertical" />
-          <p className="text-[#7E7A7A]">
-            For new home construction, AES is one of
-            <br />
-            {' '}
-            the best service providers
+          <p className="text-[#D3D3D3] font-bold text-xs">
+            For new home construction, AES is one of the best service providers
           </p>
         </div>
       </div>
       <div className="solar flex flex-col justify-between text-white items-center">
-        <h2 className="font-extrabold text-2xl mx-8 mt-16">
+        <h2 className="font-extrabold mx-8 mt-16">
           Whole House Generator Installation & Maintenance
         </h2>
-        <div className="mb-12">
+        <div className="flex gap-3 items-center justify-center p-8 md:p-12">
           <div className="vertical-2" />
-          <p className="text-[#7E7A7A]">
-            For new home construction, AES is one of
-            <br />
-            {' '}
-            the best service providers
+          <p className="text-[#D3D3D3] text-xs font-bold">
+            We guarantee secure installation for generators, and the post
+            installation maintenance
           </p>
         </div>
       </div>
       <div className="industrial flex flex-col justify-between text-white items-center">
-        <h2 className="font-extrabold text-2xl mx-8 mt-16">
+        <h2 className="font-extrabold mx-8 mt-16">
           Commercial & industrial Services
         </h2>
-        <div className="mb-12">
+        <div className="flex gap-3 items-center justify-center p-8 md:p-12">
           <div className="vertical-3" />
-          <p className="text-[#7E7A7A]">
-            For new home construction, AES is one of
-            <br />
-            {' '}
-            the best service providers
+          <p className="text-[#D3D3D3] font-bold text-xs">
+            Our qualified electricians have extensive experience in the
+            commercial and industrial fields
           </p>
         </div>
       </div>
